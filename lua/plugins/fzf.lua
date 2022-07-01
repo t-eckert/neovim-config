f149f810ef6c982755dfdca498c06e3fefe4b57a
Thomas Eckert <thomas.james.eckert@gmail.com>
2022-06-30 22:30:41 -0400

    Change FZF display

--- lua/plugins/fzf.lua
+++ lua/plugins/fzf.lua
@@ -9,35 +9,13 @@ local xmap = utils.xmap
 local omap = utils.omap
 local imap = utils.imap
 
-function FloatingFZF()
-  local lines = o.lines
-  local columns = o.columns
-  local buf = api.nvim_create_buf(true, true)
-  local height = fn.float2nr(lines * 0.5)
-  local width = fn.float2nr(columns * 0.7)
-  local horizontal = fn.float2nr((columns - width) / 2)
-  local vertical = 0
-  local opts = {
-    relative = "editor",
-    row = vertical,
-    col = horizontal,
-    width = width,
-    height = height,
-    style = "minimal"
-  }
-  vim.api.nvim_open_win(buf, true, opts)
-end
-
 local fzf_opts = {
   env.FZF_DEFAULT_OPTS or "",
   " --layout=reverse",
   ' --pointer=" "',
-  " --info=hidden",
+  " --info=inline",
   " --border=rounded",
-  " --bind å:select-all+accept"
 }
 
 env.FZF_DEFAULT_OPTS = table.concat(fzf_opts, "")
 
-g.fzf_preview_window = {"right:50%:hidden", "?"}
-g.fzf_layout = {window = "call v:lua.FloatingFZF()"}
