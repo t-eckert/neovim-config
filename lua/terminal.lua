e5bba81f656100f21c6cd2cda7cee33b2ab7cca1
Thomas Eckert <thomas.james.eckert@gmail.com>
2022-08-29 09:10:20 -0400

    Have terminal open up horizontal split

--- lua/terminal.lua
+++ lua/terminal.lua
@@ -8,17 +8,9 @@ require("toggleterm").setup({
 	start_in_insert = true,
 	insert_mappings = true,
 	persist_size = true,
-	direction = "float",
+	direction = "horizontal",
 	close_on_exit = true,
 	shell = vim.o.shell,
-	float_opts = {
-		border = "curved",
-		winblend = 0,
-		highlights = {
-			border = "Normal",
-			background = "Normal",
-		},
-	},
 })
 
 function _G.set_terminal_keymaps()
