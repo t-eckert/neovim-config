1b691d191b30b2764669ee87af64e7885fa2cf32
Thomas Eckert <teckert@hashicorp.com>
2022-10-03 09:49:46 -0400

    Remove smooth scrolling. Add vertical bars.

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -24,7 +24,7 @@ plugBegin("~/.config/nvim/plugged")
 	Plug("akinsho/toggleterm.nvim", {["tag"] = "v2.*"})
 	Plug "dhruvasagar/vim-table-mode" -- For creating great tables in md
 	Plug "sheerun/vim-polyglot" -- Syntax support
-	Plug "karb94/neoscroll.nvim" -- Smooth scrolling
+	Plug "lukas-reineke/indent-blankline.nvim" -- Show vertical indent lines
 
 	Plug "hrsh7th/nvim-cmp" -- Completion
 	Plug "hrsh7th/cmp-nvim-lsp" -- Completion LSP 
@@ -70,16 +70,3 @@ require("nvim-treesitter.configs").setup({
 	auto_install = true,
 })
 
-local timing = "10"
-require("neoscroll").setup()
-local t = {}
-t['<C-u>'] = {'scroll', {'-vim.wo.scroll', 'true', timing}}
-t['<C-d>'] = {'scroll', { 'vim.wo.scroll', 'true', timing}}
-t['<C-b>'] = {'scroll', {'-vim.api.nvim_win_get_height(0)', 'true', timing}}
-t['<C-f>'] = {'scroll', { 'vim.api.nvim_win_get_height(0)', 'true', timing}}
-t['<C-y>'] = {'scroll', {'-0.10', 'false', timing}}
-t['<C-e>'] = {'scroll', { '0.10', 'false', timing}}
-t['zt']    = {'zt', {timing}}
-t['zz']    = {'zz', {timing}}
-t['zb']    = {'zb', {timing}}
-require("neoscroll.config").set_mappings(t)
