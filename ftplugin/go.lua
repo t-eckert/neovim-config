3f262a2a7b74476d7a8bbb639b9ff6371f1644b7
Thomas Eckert <thomas.james.eckert@gmail.com>
2022-12-07 10:36:27 -0500

    Last few debugging things

--- ftplugin/go.lua
+++ ftplugin/go.lua
@@ -2,3 +2,4 @@ local nnoremap = require("utils").nnoremap
 
 nnoremap("<leader>dt", ":lua require('dap-go').debug_test()<CR>")
 nnoremap("<leader>dl", ":lua require('dap-go').debug_last_test()<CR>")
+
--- lua/debugging.lua
+++ lua/debugging.lua
@@ -1,6 +1,9 @@
 require("dapui").setup() -- User interface for debugging
 require("nvim-dap-virtual-text").setup() -- Show types and values while debugging
 
+vim.fn.sign_define('DapBreakpoint', {text='🛑'})
+vim.fn.sign_define('DapBreakpointCondition', {text='🛑'})
+
 -- Automatically start the UI when debugging
 local dap, ui = require("dap"), require("dapui")
 dap.listeners.after.event_initialized["dapui_config"] = function()
