b0bb98d3318712fb8a4e6cd8a32eabfff0e075fe
Thomas Eckert <thomas.james.eckert@gmail.com>
2023-01-16 16:09:57 -0500

    Remove resizing mappings

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -31,12 +31,6 @@ nnoremap("<C-K>", "<C-W>k")
 nnoremap("<C-L>", "<C-W>l")
 nnoremap("<C-H>", "<C-W>h")
 
--- Resize Panes with SHIFT+CTRL+direction
-nnoremap("<C-S-J>", ":")
-nnoremap("<C-S-K>", "")
-nnoremap("<C-S-L>", "")
-nnoremap("<C-S-H>", "")
-
 -- Change tabs with tab
 nnoremap("<TAB>", "gt")
 nnoremap("<S-TAB>", "gT")
