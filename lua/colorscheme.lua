2fa9eaf3b02036dc980d2c5feda1491f575fa3a2
Thomas Eckert <thomas.james.eckert@gmail.com>
2023-01-16 16:10:11 -0500

    Black background for virtual text

--- lua/colorscheme.lua
+++ lua/colorscheme.lua
@@ -5,3 +5,7 @@ require("nightfox").setup({
 })
 
 vim.cmd("colorscheme duskfox")
+vim.cmd("hi DiagnosticVirtualTextError guibg=#000000")
+vim.cmd("hi DiagnosticVirtualTextWarn guibg=#000000")
+vim.cmd("hi DiagnosticVirtualTextInfo guibg=#000000")
+vim.cmd("hi DiagnosticVirtualTextHint guibg=#000000")
