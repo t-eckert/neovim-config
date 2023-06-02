70580f2eac4852df4507e070c311140dd4875d8d
Thomas Eckert <thomas.james.eckert@gmail.com>
2023-06-01 22:03:26 -0400

    Add prisma plugin

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -9,81 +9,84 @@ plugBegin("~/.config/nvim/plugged")
 
 -- Language Support
 Plug("nvim-treesitter/nvim-treesitter", { ["do"] = ":TSUpdate" }) -- Treesitter
-Plug "neovim/nvim-lspconfig" -- Language server
-Plug "williamboman/mason.nvim" -- Installer for LSPs
-Plug "williamboman/mason-lspconfig.nvim" -- Manages LSP installations
-Plug "VonHeikemen/lsp-zero.nvim" -- Configure LSPs
+Plug "neovim/nvim-lspconfig"                                      -- Language server
+Plug "williamboman/mason.nvim"                                    -- Installer for LSPs
+Plug "williamboman/mason-lspconfig.nvim"                          -- Manages LSP installations
+Plug "VonHeikemen/lsp-zero.nvim"                                  -- Configure LSPs
 
 -- Helpers
 Plug "nvim-lua/plenary.nvim" -- Lua helpers that are used by other plugins
 
 -- UI
-Plug "kyazdani42/nvim-web-devicons" -- Icons
-Plug "kyazdani42/nvim-tree.lua" -- File tree
-Plug "nvim-lualine/lualine.nvim" -- Status line
-Plug "nanozuki/tabby.nvim" -- Tabline
-Plug "SmiteshP/nvim-navic" -- Breadcrumbs
-Plug "ray-x/guihua.lua" -- GUI management
+Plug "kyazdani42/nvim-web-devicons"                          -- Icons
+Plug "kyazdani42/nvim-tree.lua"                              -- File tree
+Plug "nvim-lualine/lualine.nvim"                             -- Status line
+Plug "nanozuki/tabby.nvim"                                   -- Tabline
+Plug "SmiteshP/nvim-navic"                                   -- Breadcrumbs
+Plug "ray-x/guihua.lua"                                      -- GUI management
 Plug("nvim-telescope/telescope.nvim", { ["tag"] = "0.1.0" }) -- Modal search
-Plug("akinsho/toggleterm.nvim", { ["tag"] = "v2.*" }) -- Toggleable terminal
+Plug("akinsho/toggleterm.nvim", { ["tag"] = "v2.*" })        -- Toggleable terminal
 
-Plug "notjedi/nvim-rooter.lua" -- Change working directory to root on startup
+Plug "notjedi/nvim-rooter.lua"                               -- Change working directory to root on startup
 
 -- Debugging
 Plug "mfussenegger/nvim-dap" -- DAP
-Plug "rcarriga/nvim-dap-ui" -- DAP UI
-Plug "leoluz/nvim-dap-go" -- Go support
+Plug "rcarriga/nvim-dap-ui"  -- DAP UI
+Plug "leoluz/nvim-dap-go"    -- Go support
 Plug "theHamsta/nvim-dap-virtual-text"
 Plug "nvim-telescope/telescope-dap.nvim"
 
 -- Snippets
-Plug "github/copilot.vim" -- Copilot
-Plug "hrsh7th/nvim-cmp" -- Completion
-Plug "hrsh7th/cmp-nvim-lsp" -- Completion LSP
-Plug "hrsh7th/cmp-buffer" -- Completion buffer
-Plug "hrsh7th/cmp-path" -- Path for CMP
-Plug "hrsh7th/cmp-cmdline" -- Command line for CMP
-Plug "L3MON4D3/LuaSnip" -- Snippet engine
-Plug "saadparwaiz1/cmp_luasnip" -- Engine plugin for CMP
+Plug "github/copilot.vim"           -- Copilot
+Plug "hrsh7th/nvim-cmp"             -- Completion
+Plug "hrsh7th/cmp-nvim-lsp"         -- Completion LSP
+Plug "hrsh7th/cmp-buffer"           -- Completion buffer
+Plug "hrsh7th/cmp-path"             -- Path for CMP
+Plug "hrsh7th/cmp-cmdline"          -- Command line for CMP
+Plug "L3MON4D3/LuaSnip"             -- Snippet engine
+Plug "saadparwaiz1/cmp_luasnip"     -- Engine plugin for CMP
 Plug "rafamadriz/friendly-snippets" -- Common, useful snippets
-Plug "mattn/emmet-vim" -- Emmet completion
+Plug "mattn/emmet-vim"              -- Emmet completion
 
 -- Formatting and code visuals
-Plug "EdenEast/nightfox.nvim" -- Color scheme
-Plug "gpanders/editorconfig.nvim" -- Editorconfig
-Plug "norcalli/nvim-colorizer.lua" -- Show hex colors
-Plug "lukas-reineke/indent-blankline.nvim" -- Show vertical indent lines
-Plug "lewis6991/gitsigns.nvim" -- Git gutter
-Plug "sheerun/vim-polyglot" -- Syntax support
-Plug "edluffy/hologram.nvim" -- Display images
-Plug "psf/black" -- Python formatter
-Plug "fatih/vim-go" -- Go support
-Plug "imsnif/kdl.vim" -- KDL support
-Plug "ray-x/go.nvim" -- More Go support
-Plug "JulesWang/css.vim" -- CSS support
-Plug "alexlafroscia/postcss-syntax.vim" -- PostCSS syntax highlighting
-Plug "stephenway/postcss.vim" -- PostCSS support
-Plug "evanleck/vim-svelte" -- Svelte
-Plug "wuelnerdotexe/vim-astro" -- Astro support
+Plug "EdenEast/nightfox.nvim"                      -- Color scheme
+Plug "gpanders/editorconfig.nvim"                  -- Editorconfig
+Plug "norcalli/nvim-colorizer.lua"                 -- Show hex colors
+Plug "lukas-reineke/indent-blankline.nvim"         -- Show vertical indent lines
+Plug "lewis6991/gitsigns.nvim"                     -- Git gutter
+Plug "sheerun/vim-polyglot"                        -- Syntax support
+Plug "edluffy/hologram.nvim"                       -- Display images
+Plug "psf/black"                                   -- Python formatter
+Plug "fatih/vim-go"                                -- Go support
+Plug "imsnif/kdl.vim"                              -- KDL support
+Plug "ray-x/go.nvim"                               -- More Go support
+Plug "JulesWang/css.vim"                           -- CSS support
+Plug "alexlafroscia/postcss-syntax.vim"            -- PostCSS syntax highlighting
+Plug "stephenway/postcss.vim"                      -- PostCSS support
+Plug "evanleck/vim-svelte"                         -- Svelte
+Plug "wuelnerdotexe/vim-astro"                     -- Astro support
 Plug "JoosepAlviste/nvim-ts-context-commentstring" -- TS comment context
-Plug("gregsexton/MatchTag", { ["for"] = "html" }) -- match tags in html
-Plug("othree/html5.vim", { ["for"] = "html" }) -- html5 support
-Plug 'pangloss/vim-javascript' -- Javascript support
-Plug "mzlogin/vim-markdown-toc" -- Markdown table of contents
-Plug 'HerringtonDarkholme/yats.vim' -- Typescript support
+Plug("gregsexton/MatchTag", { ["for"] = "html" })  -- match tags in html
+Plug("othree/html5.vim", { ["for"] = "html" })     -- html5 support
+Plug 'pangloss/vim-javascript'                     -- Javascript support
+Plug "mzlogin/vim-markdown-toc"                    -- Markdown table of contents
+Plug 'HerringtonDarkholme/yats.vim'                -- Typescript support
+Plug "prisma/vim-prisma"                           -- Prisma support
 Plug("prettier/vim-prettier",
-	{ ["do"] = "yarn install --frozen-lockfile --production",
-		["for"] = { "javascript", "typescript", "css", "less", "scss", "json", "graphql", "vue", "svelte", "html" } }) -- Prettier
+	{
+		["do"] = "yarn install --frozen-lockfile --production",
+		["for"] = { "javascript", "typescript", "css", "less", "scss", "json", "graphql", "vue", "svelte", "html" }
+	})                                                                                                           -- Prettier
 
 -- Editing
-Plug "tpope/vim-abolish" -- Better replacement
-Plug "sharkdp/fd" -- Find alternative
-Plug "windwp/nvim-autopairs" -- Autoclose
+Plug "tpope/vim-abolish"          -- Better replacement
+Plug "sharkdp/fd"                 -- Find alternative
+Plug "windwp/nvim-autopairs"      -- Autoclose
 Plug "dhruvasagar/vim-table-mode" -- For creating great tables in md
-Plug "kburdett/vim-nuuid" -- Insert new UUID on <leader>u
-Plug "tpope/vim-fugitive" -- Git support
-Plug "tpope/vim-commentary" -- Comment out multiple lines with gcc
-Plug "godlygeek/tabular" -- Alignment
+Plug "kburdett/vim-nuuid"         -- Insert new UUID on <leader>u
+Plug "tpope/vim-fugitive"         -- Git support
+Plug "tpope/vim-commentary"       -- Comment out multiple lines with gcc
+Plug "godlygeek/tabular"          -- Alignment
 
 -- Misc
 Plug "wakatime/vim-wakatime" -- Wakatime
